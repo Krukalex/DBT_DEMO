f075cc7f0e2c0ca635e7bcc612d29a84871264fa
Alex Kruk <krukalex99@gmail.com>
2026-05-28 00:20:04 +0000

    trip_fact model created

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -1,16 +1,12 @@
-WITH BIKE as (
-    select 
-        distinct
-        start_statio_id as start_station_id,
-        start_station_name,
-        start_lat,
-        start_lng
-    from 
-        {{ source('demo', 'bike') }}
-    WHERE
-        RIDE_ID != 'ride_id'
-)
-select
-    *
-FROM
-    bike
+with
+    bike as (
+        select distinct
+            start_statio_id as start_station_id,
+            start_station_name,
+            start_lat,
+            start_lng
+        from {{ source("demo", "bike") }}
+        where ride_id != 'ride_id'
+    )
+select *
+from bike
